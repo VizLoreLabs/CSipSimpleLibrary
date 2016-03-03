e1319ba526431bab2b98e68c2a6c0b78db88423d
Ognjen Ikovic <ognjen.ikovic@vizlore.com>
2016-03-03 15:17:52 +0100

    First commit; Adjusted CSipSimple to build it as AAR library without GUI

---
+++ app/src/main/java/com/csipsimple/utils/wizards/WizardUtils.java
@@ -0,0 +1,18 @@
+package com.csipsimple.utils.wizards;
+
+/**
+ * Created by ognjen on 3.2.16..
+ */
+public class WizardUtils {
+    public static final String LABEL = "LABEL";
+    public static final String ICON = "ICON";
+    public static final String ID = "ID";
+    public static final String LANG_DISPLAY = "DISPLAY";
+    public static final String PRIORITY = "PRIORITY";
+    public static final String PRIORITY_INT = "PRIORITY_INT";
+
+    public static final String EXPERT_WIZARD_TAG = "EXPERT";
+    public static final String BASIC_WIZARD_TAG = "BASIC";
+    public static final String ADVANCED_WIZARD_TAG = "ADVANCED";
+    public static final String LOCAL_WIZARD_TAG = "LOCAL";
+}
